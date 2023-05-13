aafa907c5141c7872a353a329a7032f2fbdda72c
Gautampandeyji <gautampandey076@gmail.com>
2023-05-13 14:22:11 +0530

    Final commit

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const uri = process.env.HABIT_URI;
+
+
+//local db settingss
+mongoose.connect('mongodb+srv://gautampandey076:JPZupzRvL3NBnOuq@cluster0.glizety.mongodb.net/?retryWrites=true&w=majority');
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'DB Error'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
